b9a3766570906320f588148f3b4b35e91e917ec1
Pardaqulov Davlatshoh <davlatshoh6275@gmail.com>
2023-04-21 16:30:20 +0300

    created redux file

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useState} from "react"
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -55,7 +55,6 @@ function createData(name, calories, fat, carbs, protein) {
 //   createData("Oreo", 437, 18.0, 63, 4.0),
 // ];
 
-
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -65,7 +64,6 @@ function descendingComparator(a, b, orderBy) {
   }
   return 0;
 }
-
 
 function getComparator(order, orderBy) {
   return order === "desc"
@@ -139,10 +137,6 @@ function EnhancedTableHead(props) {
     onRequestSort(event, newOrderBy);
   };
 
-
-
-
-
   return (
     <TableHead>
       <TableRow>
@@ -193,11 +187,7 @@ EnhancedTableHead.propTypes = {
 };
 
 function EnhancedTableToolbar(props) {
-
-
-
   const { numSelected } = props;
-
 
   return (
     <Toolbar
@@ -238,7 +228,7 @@ function EnhancedTableToolbar(props) {
       </IconButton>
       {numSelected > 0 ? (
         <Tooltip title="Delete">
-          <IconButton >
+          <IconButton>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
@@ -257,23 +247,22 @@ EnhancedTableToolbar.propTypes = {
   numSelected: PropTypes.number.isRequired,
 };
 
-export default function Content( { value }) {
-
+export default function Content({ value }) {
   const rows = [
-      createData("Cupcake", 305, 3.7, 67, 4.3),
-      createData("Donut", 452, 25.0, 51, 4.9),
-      createData("Eclair", 262, 16.0, 24, 6.0),
-      createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-      createData("Gingerbread", 356, 16.0, 49, 3.9),
-      createData("Honeycomb", 408, 3.2, 87, 6.5),
-      createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-      createData("Jelly Bean", 375, 0.0, 94, 0.0),
-      createData("KitKat", 518, 26.0, 65, 7.0),
-      createData("Lollipop", 392, 0.2, 98, 0.0),
-      createData("Marshmallow", 318, 0, 81, 2.0),
-      createData("Nougat", 360, 19.0, 9, 37.0),
-      createData("Oreo", 437, 18.0, 63, 4.0),
-    ];
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Donut", 452, 25.0, 51, 4.9),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Honeycomb", 408, 3.2, 87, 6.5),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Jelly Bean", 375, 0.0, 94, 0.0),
+    createData("KitKat", 518, 26.0, 65, 7.0),
+    createData("Lollipop", 392, 0.2, 98, 0.0),
+    createData("Marshmallow", 318, 0, 81, 2.0),
+    createData("Nougat", 360, 19.0, 9, 37.0),
+    createData("Oreo", 437, 18.0, 63, 4.0),
+  ];
 
   const [order, setOrder] = React.useState(DEFAULT_ORDER);
   const [orderBy, setOrderBy] = React.useState(DEFAULT_ORDER_BY);
@@ -284,14 +273,9 @@ export default function Content( { value }) {
   const [rowsPerPage, setRowsPerPage] = React.useState(DEFAULT_ROWS_PER_PAGE);
   const [paddingHeight, setPaddingHeight] = React.useState(0);
 
-
-
-
-  const filter = rows.filter(item => {
-    return item.name.toLowerCase().includes(value.toLowerCase())
-  })
-
-
+  const filter = rows.filter((item) => {
+    return item.name.toLowerCase().includes(value.toLowerCase());
+  });
 
   // modal
 
